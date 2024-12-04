4efefef559253aa6fc92af8883968ccca7835e55
Ričards Grīnbergs <ric.grinbergs@gmail.com>
2024-12-04 20:08:37 +0200

    Day 3.2 tests

--- src/solutions/day-3/solution.test.ts
+++ src/solutions/day-3/solution.test.ts
@@ -3,21 +3,21 @@ import { expect } from "jsr:@std/expect";
 import Solution from "./solution.ts";
 
 describe("day 3 solution", () => {
-  let input: string;
-
-  before(async () => {
-    input = await Deno.readTextFile(
+  test("part 1", async () => {
+    const input = await Deno.readTextFile(
       `./src/solutions/day-3/test-input.txt`,
     );
-  });
 
-  test("part 1", () => {
     const result = new Solution().part1(input);
 
     expect(result).toEqual("161");
   });
 
-  test("part 2", () => {
+  test("part 2", async () => {
+    const input = await Deno.readTextFile(
+      `./src/solutions/day-3/test-input-2.txt`,
+    );
+
     const result = new Solution().part2(input);
 
     expect(result).toEqual("48");
